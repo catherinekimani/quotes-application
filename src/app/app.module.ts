1aff4253d819741e6e46d33c5fa14d50add62b4c
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-04-08 19:04:19 +0300

    Clear app component page, Create quote class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoComponentComponent } from './logo-component/logo-component.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponentComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
