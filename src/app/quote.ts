23e7f289d54ffa950c0f304d3ee777271d3af708
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-04-09 03:39:46 +0300

    ft: add class properties

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,18 @@
 export class Quote {
+    // quote class
+    id: number;
+    title: string;
+    author: string;
+    quote: string;
+    upvotes: number;
+    downvotes: number;
+    // constructor
+    constructor(id: number, title: string, author: string, quote: string, upvotes: number, downvotes: number) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.quote = quote;
+        this.upvotes = upvotes;
+        this.downvotes = downvotes
+    }
 }
