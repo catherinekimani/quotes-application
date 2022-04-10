6cdc163930abd20102562e8eb66ab7fcff1f2d03
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-04-10 03:42:12 +0300

    Add slight changes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,10 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, "Tech", "With supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional content..", "By: Catherine Kimani", 0, 0),
-    new Quote(2, "Tech", "With supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional content.", " By: Diana Kariuki", 0, 0),
-    new Quote(3, "Tech", "With supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional content.", " By: Nikas Nganga", 0, 0),
-    new Quote(4, "Tech", "With supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional content.", "By: Benard Kimani", 0, 0),
+    new Quote(1, "FunnyTechQuote", "“Technology can be cool. I mean, think about it: when you see a dishwasher working or an iPhone loading up the next song on its playlist for your jog in the park  doesnt that feel like magic?”.", "By: Catherine Kimani", 0, 0),
+    new Quote(2, "InspiringTechQuote", "“Technology is the future of learning. Students and teachers need to be able to use it in every class because technology provides an interactive platform for teaching material that students can understand better and gives them a new way of interacting with classmates from all over the world.”", " By: Diana Kariuki", 0, 0),
+    new Quote(3, "MotivationalTechQuote", "If future generations are to remember us more with gratitude than sorrow, we must achieve more than just the miracles of technology. We must also leave them a glimpse of the world as it was created, not just as it looked when we got through with it.", " By: Nikas Nganga", 0, 0),
+    new Quote(4, "TechQuote", "Technology has made our world so much smaller, that we can now learn about other cultures or countries with ease thanks to Youtube videos and online news stories!”", "By: Benard Kimani", 0, 0),
   ];
 
   addNewQuote(quote:Quote) {
